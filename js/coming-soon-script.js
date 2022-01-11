135afdb1a7f8a10221a82c799dd38d62d0fd5289
？？？？ <yinyf_napol@163.com>
2022-01-11 16:45:44 +0800

    计时更新

--- js/coming-soon-script.js
+++ js/coming-soon-script.js
@@ -1,14 +1,39 @@
-$(function() {
-    // comming count down clock
-    $('#clock').countDown({  
-        targetDate: {
-            'day'   : 1,
-            'month' : 6,
-            'year'  : 2016,
-            'hour'  : 0,
-            'min'   : 0,
-            'sec'   : 0
-        },
-        omitWeeks: true
-    });
-});
+var clock;
+
+var loveDate = new Date("2022/01/08 12:00:00");
+
+timer = setInterval(function() {
+  timeBetweenDates(loveDate);
+}, 1000);
+
+function timeBetweenDates(loveDate) {
+  var now = new Date();
+  var difference = now.getTime() - loveDate.getTime();
+
+  if (difference <= 0) {
+    // Timer done
+    clearInterval(clock);
+  } else {
+    var seconds = Math.floor(difference / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    hours = PrefixInteger(hours, 2);
+    minutes = PrefixInteger(minutes, 2);
+    seconds = PrefixInteger(seconds, 2);
+
+    $("#days").text(days);
+    $("#hours").text(hours);
+    $("#minutes").text(minutes);
+    $("#seconds").text(seconds);
+  }
+}
+
+function PrefixInteger(num, length) {
+    return (Array(length).join('0') + num).slice(-length);
+  }
